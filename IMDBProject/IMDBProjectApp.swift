ec01a50c25a1fbc7b728a6973351a70561d8dd2c
Gal Linial <galinial@hotmail.com>
2024-04-04 21:37:19 +0300

    Initial Commit

---
+++ IMDBProject/IMDBProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDBProjectApp.swift
+//  IMDBProject
+//
+//  Created by gal linial on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct IMDBProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
